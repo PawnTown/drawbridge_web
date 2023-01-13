5fb61301a93c653b0a1b61fcdbc88c60c269f55d
Khadim Fall <khadimfall0602@gmail.com>
2023-01-13 01:10:56 +0100

    Updates

---
+++ app/immutable/chunks/0-6452c827.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-d0766028.js";export{t as component};
---
+++ app/immutable/chunks/2-bf2b521c.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/_page.svelte-5793075b.js";export{t as component,r as universal};
